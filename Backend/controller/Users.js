6dfe9b7b41722de672a780f241fb3b3f3c126522
sriman09 <39696623+sriman09@users.noreply.github.com>
2023-12-16 11:20:36 +0530

    password encryption added

--- Backend/controller/Users.js
+++ Backend/controller/Users.js
@@ -1,4 +1,11 @@
 const Users = require("../models/Users");
+const bcrypt = require("bcrypt");
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+  return hashPassword;
+};
 
 const registerUser = async (req, res) => {
   try {
@@ -8,14 +15,38 @@ const registerUser = async (req, res) => {
         message: "Please provide all the details",
       });
     } else {
-      const user = new Users({ firstName, lastName, email, password, type });
+      const hasedPassword = await hashPassword(password);
+      const user = new Users({
+        firstName,
+        lastName,
+        email,
+        password: hasedPassword,
+        type,
+      });
       await user.save();
       res.status(200).json({
         message: "User Register Successfully",
       });
     }
   } catch (err) {
+    console.log(err);
     res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email && !password) {
+      return res.status(400).json({
+        message: "Please provide required details.",
+      });
+    } else {
+    }
+  } catch (err) {
+    res.status.json({
       message: "Internal Server Error",
     });
   }
