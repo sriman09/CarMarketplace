e2239de497d28ddfdc334da6446053599eb213d7
sriman09 <39696623+sriman09@users.noreply.github.com>
2023-12-26 19:20:38 +0530

    feat: Link added in the navigation

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import React from "react";
 import logo from "../../public/assets/bbt-logo.png";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <>
       <div className="px-10 md:px-28 py-5 flex justify-between bg-black">
-        <Image src={logo} alt="logo" width={120} height={120} />
+        <Link href={"/"}>
+          <Image src={logo} alt="logo" width={120} height={120} />
+        </Link>
         <ul className="hidden md:flex flex-row gap-4 text-white items-center">
-          <li>Buy Car</li>
-          <li>Sell Car</li>
-          <li>EMI Calculator</li>
-          <li>Contact Us</li>
+          <Link href={"/collections"}>Buy Car</Link>
+          <Link href={"/sell-cars"}>Sell Car</Link>
+          <Link href={"/emi-calculator"}>EMI Calculator</Link>
+          <Link href={"/contact"}>Contact Us</Link>
         </ul>
       </div>
     </>
