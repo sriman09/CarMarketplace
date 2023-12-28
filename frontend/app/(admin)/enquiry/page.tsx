46c171c94b584c3f6d66856f93034a9a701e68a8
sriman09 <39696623+sriman09@users.noreply.github.com>
2023-12-28 12:03:53 +0530

    change

--- frontend/app/(admin)/enquiry/page.tsx
+++ frontend/app/(admin)/enquiry/page.tsx
@@ -1,5 +1,21 @@
-function page() {
-  return <div>Enquiry</div>;
+"use client";
+import React, { useRef } from "react";
+import BackButton from "../components/BackButton";
+import SearchBar from "../components/SearchBar";
+import BlueButton from "../components/BlueButton";
+
+function Page() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  return (
+    <>
+      <BackButton back={false} />
+      <div className="flex flex-col mt-5 md:mt-10">
+        <div className="flex flex-row justify-between">
+          <SearchBar placeholder="Search Enquiry" inputRef={inputRef} />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default page;
+export default Page;
--- frontend/app/(admin)/inventory/page.tsx
+++ frontend/app/(admin)/inventory/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import BackButton from "../components/BackButton";
+import SearchBar from "../components/SearchBar";
+import BlueButton from "../components/BlueButton";
 
 function Page() {
-  return <div>Inventory</div>;
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  return (
+    <>
+      <BackButton back={false} />
+      <div className="flex flex-col mt-5 md:mt-10">
+        <div className="flex flex-row justify-between">
+          <SearchBar placeholder="Search Inventory" inputRef={inputRef} />
+          <BlueButton>Create</BlueButton>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Page;
