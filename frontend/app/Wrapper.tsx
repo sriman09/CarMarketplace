b68bffe4e803052750f965e5b696c9a0faffef6c
sriman09 <39696623+sriman09@users.noreply.github.com>
2024-01-18 16:30:30 +0530

    speed insight added

--- frontend/app/Wrapper.tsx
+++ frontend/app/Wrapper.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import ReactModal from "react-modal";
 import { ToastContainer } from "react-toastify";
 import { RecoilRoot } from "recoil";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function Wrapper({ children }: { children: React.ReactNode }) {
   return (
     <>
       <ToastContainer />
+      <SpeedInsights />
       <RecoilRoot>{children}</RecoilRoot>
     </>
   );
