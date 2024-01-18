67446f725ffb0310e3aafa94f0ae58ffe885dac0
sriman09 <39696623+sriman09@users.noreply.github.com>
2024-01-18 16:33:07 +0530

    web analytics added

--- frontend/app/Wrapper.tsx
+++ frontend/app/Wrapper.tsx
@@ -5,12 +5,14 @@ import ReactModal from "react-modal";
 import { ToastContainer } from "react-toastify";
 import { RecoilRoot } from "recoil";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function Wrapper({ children }: { children: React.ReactNode }) {
   return (
     <>
       <ToastContainer />
       <SpeedInsights />
+      <Analytics />
       <RecoilRoot>{children}</RecoilRoot>
     </>
   );
