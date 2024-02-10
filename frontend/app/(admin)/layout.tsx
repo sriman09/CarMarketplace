62eeb0c4b05100d63bad6aa9a8eb3a031073d06a
sriman09 <39696623+sriman09@users.noreply.github.com>
2024-02-10 23:13:51 +0530

    mobile navigation added

--- frontend/app/(admin)/layout.tsx
+++ frontend/app/(admin)/layout.tsx
@@ -41,7 +41,27 @@ export default function AuthLayout({
       </div>
       <div className="w-full md:w-3/4 flex flex-col bg-[#EFF6FF] overflow-y-scroll">
         <AdminHeader />
-        <div className="px-2 md:px-8 lg:px-20">{children}</div>
+        <div className="px-2 pb-20 md:pb-0 md:px-8 lg:px-20">{children}</div>
+      </div>
+      {/* Mobile Bottom Bar */}
+      <div className="md:hidden bg-white fixed bottom-0 left-0 w-full flex justify-around pb-2 border-t shadow-2xl">
+        {adminRoutes.map((item) => (
+          <button
+            onClick={() => handleSideButtonClick(item)}
+            key={item.id}
+            id={item.id}
+            className={`flex gap-3 px-4 py-2 ${
+              activeTab === item.id
+                ? " border-t-2 border-indigo-500"
+                : "text-[#8C8C8C] grayscale"
+            }`}
+          >
+            <Image src={item.logo} alt={item.id} height={20} width={20} />
+            {/* <span className="text-xs">
+              {item.id.charAt(0).toUpperCase() + item.id.slice(1)}
+            </span> */}
+          </button>
+        ))}
       </div>
     </div>
   );
